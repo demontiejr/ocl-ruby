359badf5e3bf576c541cf1089f5ff6fcd6a0c7af
savyoins@gmail.com <savyoins@gmail.com@790e8c4c-fb82-03bb-6259-3fb92f98b3f7>
2011-04-15 17:41:37 +0000

    OPA

--- trunk/ocl-ruby/ProjetoCompiladoresOCL_Ruby/src/compilador/Main.java
+++ trunk/ocl-ruby/ProjetoCompiladoresOCL_Ruby/src/compilador/Main.java
@@ -1,19 +1,19 @@
 package compilador;
 
 
-/*********************************************************
-* Universidade Federal de Campina Grande - UFCG          *
-* Centro de Engenharia Eletrica e Informatica - CEEI     *
-* Departamento de Sistemas e Computacao - DSC            *
-*                                                        *
-* Projeto da disciplina de Compiladores, 2011.1          *
-* Compilador de Pre e Pos condicoes para verificacao     *
-* OCL para Ruby  (GUI)                                   *
-*                                                        *
-* Grupo: Francisco Demontie dos Santos Junior - 20911084 *
-*        Izabela Vanessa de Almeida Melo - 20811018      *
-*        Savyo Igor da Nobrega Santos - 20811034         *
-*********************************************************/
+/**********************************************************
+* Universidade Federal de Campina Grande - UFCG           *
+* Centro de Engenharia Eletrica e Informatica - CEEI      *
+* Departamento de Sistemas e Computacao - DSC             *
+*                                                         *
+* Projeto da disciplina de Compiladores, 2011.1           *
+* Compilador de Pre e Pos condicoes para verificacao      *
+* OCL para Ruby  (GUI)                                    *
+*                                                         *
+* Grupo: Francisco Demontie dos Santos Junior - 20911084  *
+*        Izabela Vanessa de Almeida Melo - 20811018       *
+*        Savyo Igor da Nobrega Santos - 20811034          *
+**********************************************************/
 
 public class Main {
 
