8d71e31428a397d89d40a19b4590bbff7a0a0539
demontiejunior@gmail.com <demontiejunior@gmail.com@790e8c4c-fb82-03bb-6259-3fb92f98b3f7>
2011-05-05 23:20:03 +0000

    Atualizando interface grafica

--- trunk/ocl-ruby/ProjetoCompiladoresOCL_Ruby/src/compilador/gui/MainFrame.java
+++ trunk/ocl-ruby/ProjetoCompiladoresOCL_Ruby/src/compilador/gui/MainFrame.java
@@ -3,11 +3,28 @@
 
 package compilador.gui;
 
-import JFlex.Out;
-
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Dimension;
+import java.awt.FileDialog;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Insets;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.SystemColor;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.TextEvent;
+import java.awt.event.TextListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.List;
+import java.util.Set;
+
+import JFlex.Out;
 
 /*********************************************************
 * Universidade Federal de Campina Grande - UFCG          *
@@ -27,7 +44,7 @@ final public class MainFrame extends Frame implements Handles {
   private volatile boolean choosing;
 
   private String fileName = "";
-  private String dirName = "";
+  private String xmiName = "";
   
   private Button quit; 
   private Button options;
@@ -49,6 +66,10 @@ final public class MainFrame extends Frame implements Handles {
   public final int LEXICAL = 1, SYNTACTIC = 2;
   
   public int analysisType = 2;
+  
+  private Button specChooseXmi;
+  
+  private TextField specXmi;
   
   public MainFrame() {
     super("Compilador OCL para Ruby ");
@@ -73,7 +94,9 @@ final public class MainFrame extends Frame implements Handles {
     options    = new Button("Opcoes");
     about       = new Button("Sobre");
     specChoose = new Button("Procurar");
-    spec       = new TextField(10);            
+    spec       = new TextField(10);
+    specChooseXmi = new Button("Procurar");
+    specXmi       = new TextField(10);
     messages   = new TextArea(10,80);
 
     messages.setEditable(false);
@@ -128,6 +151,25 @@ final public class MainFrame extends Frame implements Handles {
       }
     } );
     
+    specChooseXmi.addActionListener( new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          specChooseXmi();
+        }
+      } );
+
+      specXmi.addActionListener( new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          xmiName = specXmi.getText();
+          analyse();
+        }
+      } );
+      
+      specXmi.addTextListener( new TextListener() {
+        public void textValueChanged(TextEvent e) {
+          xmiName = specXmi.getText();
+        }
+      } );
+    
     GridPanel north = new GridPanel(5,4,10,10);
     north.setInsets( new Insets(10,5,5,10) );
 
@@ -139,6 +181,9 @@ final public class MainFrame extends Frame implements Handles {
     north.add( 0,0, BOTTOM, new Label("Arquivo para analise:"));
     north.add( 0,1, 2,1, spec);
     north.add( 2,1, specChoose);
+    north.add( 0,2, BOTTOM, new Label("Arquivo XMI:"));
+    north.add( 0,3, 2,1, specXmi);
+    north.add( 2,3, specChooseXmi);
 
     Panel center = new Panel(new BorderLayout());   
     center.add("North", new Label("Saida:"));
@@ -170,6 +215,8 @@ final public class MainFrame extends Frame implements Handles {
     generate.setEnabled( !generating );
     specChoose.setEnabled( !generating );
     spec.setEnabled( !generating );
+    specChooseXmi.setEnabled( !generating );
+    specXmi.setEnabled( !generating );
   }
 
   private void analyse() {
@@ -178,18 +225,21 @@ final public class MainFrame extends Frame implements Handles {
    
     setEnabledAll(true);
 
-    thread = new GeneratorThread(this, fileName, dirName);
+    thread = new GeneratorThread(this, fileName, xmiName);
     thread.start();
   } 
 
-  public void analysisFinished(boolean success, List<String> log, List<String> errors) {
+  public void analysisFinished(boolean success, List<String> log, List<String> errors, 
+		  Set<String> semanticErrors) {
     setEnabledAll(false);
     
     for (String s : log)
     	messages.append(s);
     
-    messages.append("\n"+errors.size() + " erro(s)\n");
+    messages.append("\n"+(errors.size() + semanticErrors.size()) + " erro(s)\n");
     for (String error : errors)
+    	messages.append(error);
+    for (String error : semanticErrors)
     	messages.append(error);
     
     if (success)
@@ -206,7 +256,7 @@ final public class MainFrame extends Frame implements Handles {
   private void specChoose() {
     choosing = true;
     
-    FileDialog d = new FileDialog(this, "Selecione o arquivo", FileDialog.LOAD);
+    FileDialog d = new FileDialog(this, "Selecione o arquivo OCL", FileDialog.LOAD);
     
     d.setFile("*.ocl");
     d.show();
@@ -218,6 +268,23 @@ final public class MainFrame extends Frame implements Handles {
     
     choosing = false;    
   }
+  
+	private void specChooseXmi() {
+		choosing = true;
+
+		FileDialog dxmi = new FileDialog(this, "Selecione o arquivo XMI",
+				FileDialog.LOAD);
+
+		dxmi.setFile("*.xml");
+		dxmi.show();
+
+		if (dxmi.getFile() != null) {
+			xmiName = dxmi.getDirectory() + dxmi.getFile();
+			specXmi.setText(xmiName);
+		}
+
+		choosing = false;
+	}
   
   private void about(){
 	  if (aboutPanel == null) {
