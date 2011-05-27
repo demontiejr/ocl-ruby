e2b615fb3349fb72cd51c65dd20ec072d987151c
demontiejunior@gmail.com <demontiejunior@gmail.com@790e8c4c-fb82-03bb-6259-3fb92f98b3f7>
2011-05-27 04:29:58 +0000

    tinha q dar esse commit pra n dar pau XD

--- trunk/ocl-ruby/ProjetoCompiladoresOCL_Ruby/Transacao.rb
+++ trunk/ocl-ruby/ProjetoCompiladoresOCL_Ruby/Transacao.rb
@@ -1,8 +1,16 @@
 class Transacao
-	def initialize(pontos)
+
+	def initialize(pontos, data, conta, cartao, servico)
 
 		@pontos = pontos
+		@data = data
+		@conta = conta
+		@cartao = cartao
+		@servico = servico
 
 	end
+
+	def programa() end
+
 
 end
